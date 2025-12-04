6375fc6cd54f4e06a58ebc6a302547ffd8670bf2
NattaArtemisss <m.mujib.n.r@gmail.com>
2025-12-04 14:30:22 +0700

    update

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -7,12 +7,12 @@ export const findUserByEmail = async (email) => {
   return result.rows[0];
 };
 
-export const createUser = async ({ name, email, password }) => {
+export const createUser = async ({ name, email, hashedPassword }) => {
   const result = await pool.query(
     `INSERT INTO users (name, email, password)
      VALUES ($1, $2, $3)
      RETURNING id, name, email, created_at`,
-    [name, email, password]
+    [name, email, hashedPassword]
   );
   return result.rows[0];
 };
